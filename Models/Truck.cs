3b838f7758f3e34c276a61f06d8daa3bca37a1a2
Samara.Duncan <samarasoucy@gmail.com>
2018-09-07 12:49:10 -0400

    Initial Commit

---
+++ Models/Truck.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Truck
+    {
+        public int TruckId { get; set; }
+        public int Pallets { get; set; }
+        public DateTime? EnterDCTime { get; set; }
+        public DateTime? DockStartTime { get; set; }
+        public DateTime? UnloadStartTime { get; set; }
+        public DateTime? UnloadEndTime { get; set; }
+        public DateTime? DockEndTime { get; set; }
+        public DateTime? LeaveDCTime { get; set; }
+
+    }
+}
